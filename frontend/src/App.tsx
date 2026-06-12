a61b6d65684be80d16a471105bed058f2ba1684d
Aditya Kumar <aditya.work404@gmail.com>
2026-06-12 18:10:42 +0530

    take i/p url,concurrent , duration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,71 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
-
+import { useEffect, useState , useRef } from 'react'
 
 function App() {
 
-  const [data,setData] = useState("")
+  interface liveData {
+    total : number;
+    success : number ; 
+    failure : number ;
+  }
+
+  const [data,setData] = useState<liveData>({
+    total : 0,
+    success : 0, 
+    failure : 0
+  })
+
+  const [url ,setUrl] = useState("")
+  const [concurrent , setConcurrent] = useState('')
+  const [duration , setDuration] = useState('')
   
-  async function getData(){
-    const response = await axios.get('http://localhost:8080/hello')
-    setData(response.data)
+  const wsRef = useRef<WebSocket | null>(null)
+
+  async function clickHandler(){
+    wsRef.current!.send(JSON.stringify({
+      type : "sendStats",
+      payload : {
+        url : url,
+        concurrent : concurrent,
+        duration : duration
+      }
+    }))
   }
 
   useEffect(()=> {
-    getData()
+    const socket = new WebSocket('ws://localhost:8080')
+    wsRef.current = socket
+    socket.onopen = () => {
+      console.log("Websocket connection is now Ready")
+    }
+
+    wsRef.current.onmessage = (event) => {
+      const parsedMsg = JSON.parse(event.data)
+      setData(parsedMsg.payload)
+    }
+    
   },[])
 
   return (
     <>
-      <p>TotalRequest{data}</p>
-      <p>Success</p>
-      <p>Failure</p>
+      <label htmlFor="">Webpage URL</label>
+      <input 
+      onChange={(e) => setUrl(e.target.value)}
+      className='border-2' type="text" placeholder='url' />
+
+      <p className=''>Enter No of Concurrent Users</p>
+      <input 
+      onChange={(e) => setConcurrent(e.target.value)} 
+      className='inline border-2' type="number" placeholder='No of Users'/>
+
+      <p>Duration of Test</p>
+      <input 
+      onChange={(e) => setDuration(e.target.value)}
+      className='border-2' type="number" />
+      <p>TotalRequest{data.total}</p>
+      <p>Success{data.success}</p>
+      <p>Failure{data.failure}</p>
+      <button onClick={clickHandler}>Start</button>
 
     </>
   )
