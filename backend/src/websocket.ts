1c4f5d91e13e462855636fedba70e50ced652ff4
Aditya Kumar <aditya.work404@gmail.com>
2026-06-14 18:24:00 +0530

    passing callback for live updates from loadTester

---
+++ backend/src/websocket.ts
@@ -0,0 +1,68 @@
+import type { server } from "typescript"
+import { WebSocketServer } from "ws"
+import { sendStats } from "./services/loadTester"
+
+const websocket = (httpServer : any  ) => {
+
+const wss = new WebSocketServer({server : httpServer})  // attaching websocket server to same http server
+
+interface LiveData{
+    total : number;
+    success : number ;
+    failure : number;
+}
+
+wss.on("connection",(socket) => {
+
+        console.log("user Connected")
+
+        socket.on("message",async (msg) => {
+            const parsedMsg = JSON.parse(msg.toString())
+            // console.log(parsedMsg)
+            if(parsedMsg.type == "sendStats"){
+
+                const url : string = parsedMsg.payload.url 
+                const concurrent : number = Number(parsedMsg.payload.concurrent) 
+                const duration : number = Number(parsedMsg.payload.duration)
+                
+
+                let latestStats : LiveData = { 
+                    total : 0,
+                    success : 0,
+                    failure : 0
+                }
+
+                const intervalId = setInterval(() => {
+
+                    socket.send(JSON.stringify({
+                        type: "liveStats",
+                        payload : latestStats
+                    }))
+                },1000)
+
+                const finalMetrics = await sendStats(url,
+                    concurrent,
+                    duration,
+                    (stats ) => {
+                        latestStats = stats
+                    }
+                )
+
+                clearInterval(intervalId)
+
+                socket.send(JSON.stringify({
+                    type : 'liveStats',
+                    payload : latestStats
+                }))
+
+                socket.send(JSON.stringify({
+                    type: "finalStats",
+                    payload : finalMetrics 
+                }))
+                
+            }
+        })
+    })
+}
+
+export default websocket
